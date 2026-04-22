2449eada2f9f2048fd83245a9d4d0301092dc322
pradipydv17 <pradipydv17@gmail.com>
2026-04-22 07:35:48 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def call_hello():
+    print("hello from circleci demo - Aayush")
+
+if __name__ == '__main__':
+    call_hello()
---
+++ main_test.py
@@ -0,0 +1,14 @@
+from main import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(5, 5) == 10
+    print("Add function works correctly")
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    print("Subtract function works correctly")
+
+if __name__ == '__main__':
+    test_add()
+    test_subtract()
